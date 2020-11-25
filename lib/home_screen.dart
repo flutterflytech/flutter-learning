88bd303c5a4cecb8d85df8eba58f0eef0b31f324
@alok-kumar-mobcoder <alok.kumar@mobcoder.com>
2020-11-25 17:59:10 +0530

    HomeScreen with buttons for navigating to various screen was added and code for displaying toast Message was added

---
+++ lib/home_screen.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:poc_flutter/listview_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(
+            child: Center(
+              child: Column(
+                children: [
+                  SizedBox(height: 4),
+                  Container(color: Colors.orange, child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text('Home Screen - Flutter Demo',
+                      style: TextStyle(fontWeight: FontWeight.bold),),
+                  )),
+                  RaisedButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    color: Colors.red,
+                    textColor: Colors.white,
+                    onPressed: () {
+
+                    },
+                    child: Text('Login Screen'),
+                  ),
+
+
+                  RaisedButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    color: Colors.red,
+                    textColor: Colors.white,
+                    onPressed: () {},
+                    child: Text('Splash Screen'),
+                  ),
+
+
+                  RaisedButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    color: Colors.red,
+                    textColor: Colors.white,
+                    onPressed: () {},
+                    child: Text('ListView Screen'),
+                  ),
+
+
+                  RaisedButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    color: Colors.red,
+                    textColor: Colors.white,
+                    onPressed: () {},
+                    child: Text('Dynamic ListView Screen'),
+                  ),
+
+
+                  RaisedButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    color: Colors.red,
+                    textColor: Colors.white,
+                    onPressed: () {},
+                    child: Text('ListView Screen'),
+                  ),
+
+
+                  RaisedButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    color: Colors.red,
+                    textColor: Colors.white,
+                    onPressed: () {},
+                    child: Text('ListView Screen'),
+                  ),
+
+
+                  RaisedButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    color: Colors.red,
+                    textColor: Colors.white,
+                    onPressed: () {
+                      showToastMessage();
+                    },
+                    child: Text('Show Toast'),
+                  ),
+
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+
+}
+
+void showToastMessage() {
+  Fluttertoast.showToast(
+      msg: "This is Center Short Toast",
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 1,
+      backgroundColor: Colors.grey,
+      textColor: Colors.white,
+      fontSize: 16.0
+  );
+}
